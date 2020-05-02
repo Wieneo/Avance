2dec7d0f16b479ead94a59da6ddbc8f3ead7bdf5
Johann Gnaucke <johann@gnaucke.com>
2020-05-02 20:34:11 +0200

    Started to play around with permissions

---
+++ models/templates.go
@@ -0,0 +1,58 @@
+package perms
+
+//User is the default user struct from the database
+type User struct {
+	ID          int
+	Username    string
+	Mail        string
+	Permissions Permissions
+}
+
+//Group is the default Group struct from the database
+type Group struct {
+	ID          int
+	Name        string
+	Permissions Permissions
+}
+
+//Project is the default Project struct from the database
+type Project struct {
+	ID   int
+	Name string
+}
+
+//Queue is the default Queue struct from the database
+type Queue struct {
+	ID      int
+	Name    string
+	Project Project
+}
+
+//Permissions should store all permissions regarding a user or a group
+type Permissions struct {
+	AccessTo struct {
+		Projects []ProjectPermission
+		Queues   []QueuePermission
+	}
+}
+
+//ProjectPermission stores the permissions given to a single project
+type ProjectPermission struct {
+	QueueID              int
+	CanSee               bool
+	CanModify            bool
+	CanChangePermissions bool
+	CanCreateQueues      bool
+	CanModifyQueues      bool
+	CanRemoveQueues      bool
+}
+
+//QueuePermission stores the permissions given to a single queue
+type QueuePermission struct {
+	QueueID              int
+	CanSee               bool
+	CanCreateTicket      bool
+	CanEditTicket        bool
+	CanModify            bool
+	CanChangePermissions bool
+}
