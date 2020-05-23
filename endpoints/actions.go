5977695babe32114529e237adb5ea37a4a3615d6
Johann Gnaucke <johann@gnaucke.com>
2020-05-23 18:23:53 +0200

    Updated documentation

--- endpoints/actions.go
+++ endpoints/actions.go
@@ -54,7 +54,7 @@ func CreateAction(w http.ResponseWriter, r *http.Request) {
 
 	if !allperms.Admin && !perms.CanEditTicket {
 		w.WriteHeader(403)
-		dev.ReportUserError(w, "You are not allowed to patch tickets in that queue.")
+		dev.ReportUserError(w, "You are not allowed to edit tickets in that queue.")
 		return
 	}
 
