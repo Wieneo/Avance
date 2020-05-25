b774ff764d9faa4950ded7ee5d2463c07cb80b55
Johann Gnaucke <johann@gnaucke.com>
2020-05-25 12:54:19 +0200

    MISC Changes Readded loader Made display of actions conditional (only comments and answers)

--- frontend/app/src/components/Main/ActionDisplay.vue
+++ frontend/app/src/components/Main/ActionDisplay.vue
@@ -1,9 +1,16 @@
 <template>
-    <div style="margin-top: 5px;">
-        <v-card v-for="action in CurrentTicket.Actions" :key="action.ID">
-          <v-card-subtitle><b>{{action.Title}}</b></v-card-subtitle>
-          <v-card-text v-html="action.Content"></v-card-text>
-        </v-card>
+    <div style="margin: 5px;">
+        <v-skeleton-loader
+          class="mx-auto"
+          type="text@1, paragraph@3, text@2, paragraph@1"
+          v-if="TicketLoading"
+        ></v-skeleton-loader>
+          <v-card v-for="action in CurrentTicket.Actions" :key="action.ID" style="margin-top: 5px;" v-else>
+            <div v-if="action.Type == 0 || action.Type == 1">
+              <v-card-subtitle><b>{{action.Title}}</b><br>{{action.IssuedBy.Firstname}} {{action.IssuedBy.Lastname}} ({{action.IssuedBy.Username}})<br>{{action.IssuedAt}}</v-card-subtitle>
+              <v-card-text style="color: black;" v-html="action.Content"></v-card-text>
+            </div>
+          </v-card>
     </div>
 </template>
 <script lang="ts">
