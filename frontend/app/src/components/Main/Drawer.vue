79de68c339fba9516d56eeaf9e0919e126eea488
Johann Gnaucke <johann@gnaucke.com>
2020-05-25 22:59:05 +0200

    Furhter stylized profile editor

--- frontend/app/src/components/Main/Drawer.vue
+++ frontend/app/src/components/Main/Drawer.vue
@@ -1,6 +1,5 @@
 
 <template>
-    <v-card>
     <v-navigation-drawer permanent mini-variant app
     >
         <v-list>
@@ -21,7 +20,7 @@
                         <v-card-text class="TicketDisplayProperty"><v-btn class="ma-2" outlined color="green"><v-icon style="margin-right: 10px;">mdi-circle</v-icon> Online</v-btn></v-card-text>
                     </v-card>
                 </v-col>
-                <v-btn icon>
+                <v-btn icon @click="ShowEditMenu = true">
                     <v-icon>mdi-cog</v-icon>
                 </v-btn>
             </v-row>
@@ -49,8 +48,33 @@
             <v-list-item-title>Starred</v-list-item-title>
         </v-list-item>
         </v-list>
+
+
+        <v-dialog v-model="ShowEditMenu" persistent max-width="350">
+            <v-card>
+                <v-card-title class="headline">Your Profile</v-card-title>
+                <v-card-text>
+                    <div style="text-align: center;">
+                        <v-list-item-avatar style="width: 128px; height: 128px;">
+                            <v-img class="ProfilePicture" src="https://randomuser.me/api/portraits/women/85.jpg" style="cursor: pointer; " ></v-img>
+                        </v-list-item-avatar>
+                    </div>
+                    <v-text-field label="Username" :rules="rules" hide-details="auto" v-model="ChangedProfileInfo.Username"></v-text-field>
+                    <v-text-field style="margin-top: 20px;" label="Firstname" :rules="rules" hide-details="auto" v-model="ChangedProfileInfo.Firstname"></v-text-field>
+                    <v-text-field style="margin-top: 20px;" label="Lastname" :rules="rules" hide-details="auto" v-model="ChangedProfileInfo.Lastname"></v-text-field>
+                    <v-text-field style="margin-top: 20px;" label="E-Mail" :rules="rules" hide-details="auto" v-model="ChangedProfileInfo.Mail"></v-text-field>
+                    <v-text-field style="margin-top: 25px;" label="Password" hide-details="auto" type="password" v-model="NewPassword1"></v-text-field>
+                    <span>Only needed when changing password!</span>
+                    <v-text-field label="Repeat Password" hide-details="auto" v-model="NewPassword2" type="password"></v-text-field>
+                </v-card-text>
+                <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="orange darken-1" text @click="ShowEditMenu = false">Abort</v-btn>
+                <v-btn color="green darken-1" text @click="ShowEditMenu = false">Save</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
     </v-navigation-drawer>
-    </v-card>
 </template>
 
 
@@ -72,16 +96,47 @@
       Mail: "Loading"
   }
 
+  //Initialize seperately so we don't create a reference
+  const ChangedProfileInfo: User = {
+      ID: 0,
+      Username: "Loading",
+      Firstname: "Please",
+      Lastname: "Wait",
+      Mail: "Loading"
+  }
+
   export default Vue.extend({
     name: 'Drawer',
+
     data: function(){
         return {
             UserInfo,
-            ShowUserMenu: false
+            ChangedProfileInfo,
+            NewPassword1: "",
+            NewPassword2: "",
+            ShowUserMenu: false,
+            ShowEditMenu: false,
+            rules: [
+                value => !!value || 'Required.',
+            ],
+        }
+    },
+    watch:{
+        ShowEditMenu (to){
+            if (!to){
+                //CleanUP Form
+                console.log("Resetting Inputs from Profile Edit")
+                this.ChangedProfileInfo = this.UserInfo
+                this.NewPassword1 = ""
+                this.NewPassword2 = ""
+            }
         }
     },
     mounted: async function(){
-        this.UserInfo = await Vue.prototype.$GetRequest("/api/v1/profile")
+        const data =  await Vue.prototype.$GetRequest("/api/v1/profile")
+        //Assign so we dont create a reference here
+        Object.assign(this.UserInfo, data);
+        Object.assign(this.ChangedProfileInfo, data);
     }
   })
 </script>
