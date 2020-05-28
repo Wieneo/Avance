39d02eb84054787dcb31eb33bfc042ccb3eaca48
Johann Gnaucke <johann@gnaucke.com>
2020-05-28 09:44:53 +0200

    Further sketched settings menu Created all needed vue components

--- frontend/app/src/components/Settings/Settings.vue
+++ frontend/app/src/components/Settings/Settings.vue
@@ -2,7 +2,7 @@
   <div>
     <Drawer v-on:ShowProjects="showProjects = true" />
     <div>
-      <v-tabs background-color="primary" dark height="40px" style="margin-left: 56px;">
+      <v-tabs background-color="#46494c" dark height="40px" style="margin-left: 56px;">
         <v-tab>
           <v-icon left>mdi-account</v-icon>Personal
         </v-tab>
@@ -13,8 +13,24 @@
             <v-icon left>mdi-cog</v-icon>Instance
         </v-tab>
 
-        <v-tab-item class="overflow-y-auto" style="max-height: calc(100vh - 130px);">Item1</v-tab-item>
-        <v-tab-item class="overflow-y-auto" style="max-height: calc(100vh - 130px);">Item2</v-tab-item>
+        <v-tab v-if="UserInfo.Permissions.Admin">
+            <v-icon left>mdi-human</v-icon>User & Groups
+        </v-tab>
+
+        <v-tab v-if="UserInfo.Permissions.Admin">
+            <v-icon left>mdi-ticket</v-icon>Ticket
+        </v-tab>
+
+        <v-tab v-if="UserInfo.Permissions.Admin">
+            <v-icon left>mdi-transit-connection-variant</v-icon>Integrations
+        </v-tab>
+
+        <v-tab-item class="overflow-y-auto" style="max-height: calc(100vh - 130px);"><Personal/></v-tab-item>
+        <v-tab-item class="overflow-y-auto" style="max-height: calc(100vh - 130px);"><Notifications/></v-tab-item>
+        <v-tab-item class="overflow-y-auto" style="max-height: calc(100vh - 130px);"><Instance/></v-tab-item>
+        <v-tab-item class="overflow-y-auto" style="max-height: calc(100vh - 130px);"><Users/></v-tab-item>
+        <v-tab-item class="overflow-y-auto" style="max-height: calc(100vh - 130px);"><Ticket/></v-tab-item>
+        <v-tab-item class="overflow-y-auto" style="max-height: calc(100vh - 130px);"><Integrations/></v-tab-item>
       </v-tabs>
     </div>
   </div>
@@ -23,6 +39,12 @@
 import Vue from "vue";
 import Drawer from "../Main/Drawer.vue";
 import AppBar from "../Main/AppBar.vue";
+import Personal from "./Personal.vue";
+import Ticket from "./Ticket.vue"
+import Integrations from "./Integrations.vue"
+import Notifications from "./Notifications.vue"
+import Users from "./Users.vue"
+import Instance from "./Instance.vue"
 
 interface User {
   ID: number;
@@ -46,11 +68,17 @@ const UserInfo: User = {
 export default Vue.extend({
   name: "Settings",
   components: {
-    Drawer
+    Drawer,
+    Personal,
+    Ticket,
+    Integrations,
+    Notifications,
+    Users,
+    Instance
   },
   data: function() {
     return {
-      UserInfo
+      UserInfo,
     };
   },
   mounted: async function() {
