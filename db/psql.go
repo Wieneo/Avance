cfe8dc040db7bc9874b2ea39d3f32855effc339d
Johann Gnaucke <johann@gnaucke.com>
2020-05-30 10:39:45 +0200

    Added comments

--- db/psql.go
+++ db/psql.go
@@ -160,13 +160,19 @@ func migrate() {
 		dev.LogInfo("No migrations needed!")
 	}
 	*/
+	//appliedPatches contains all patches from the "Patches" database table. These are tracked via their names
 	appliedPatches := make([]string, 0)
+	//localPatches contains all patches from the ./db/migrations/ folder. All files with the suffix .migrate.json will be included
 	localPatches := make([]Migration, 0)
+
+	//SELECT resides on the second line of this function. Is also used to check if the table even exists
 	for rows.Next() {
 		var patch string
 		rows.Scan(&patch)
 		appliedPatches = append(appliedPatches, patch)
 	}
+
+	rows.Close()
 
 	cwd, _ := os.Getwd()
 	files, err := ioutil.ReadDir(fmt.Sprint(cwd, "/db/migrations/"))
@@ -174,6 +180,7 @@ func migrate() {
 		dev.LogFatal(err, "Couldn't find/read migrations: ", err.Error())
 	}
 
+	//Read all json files
 	for _, k := range files {
 		if strings.HasSuffix(k.Name(), ".migrate.json") {
 			var migration Migration
@@ -186,11 +193,13 @@ func migrate() {
 				dev.LogFatal(err, err.Error())
 			}
 
+			//Name, Targets and After shouldn't be empty
 			if len(migration.Name) == 0 || len(migration.Targets) == 0 || len(migration.After) == 0 {
 				dev.LogWarn("Skipping empty migration: " + migration.Name + "(" + k.Name() + ")")
 				continue
 			}
 
+			//Check if the migration or a migration with that name already was included
 			found := false
 			for _, patch := range localPatches {
 				if patch.Name == migration.Name {
@@ -198,6 +207,7 @@ func migrate() {
 				}
 			}
 
+			//App should exit if migration with duplicate name exists.
 			if found {
 				dev.LogFatal(errors.New("Duplicate Migration"), "Duplicate Migartion-Name: "+migration.Name)
 			}
@@ -208,6 +218,7 @@ func migrate() {
 
 	dev.LogInfo(fmt.Sprintf("Database has %d Patches applied. Local Patches available: %d", len(appliedPatches), len(localPatches)))
 
+	//Check what patches already were applied to the database
 	neededPatches := make([]Migration, 0)
 	for _, k := range localPatches {
 		found := false
@@ -224,10 +235,14 @@ func migrate() {
 
 	dev.LogInfo(fmt.Sprintf("%d Migration(s) need to be applied", len(neededPatches)))
 
+	//MAIN APPLY
 	for len(neededPatches) > 0 {
 		patchesProcessed := 0
 		for i, k := range neededPatches {
+
+			//Check if dependency of migartion already was applied
 			baseExists := false
+			//{{BASE}} is a placeholder. Basically means the patch has no dependency and can installed right after the base image was imported
 			if k.After != "{{BASE}}" {
 				for _, db := range appliedPatches {
 					if db == k.After {
@@ -238,9 +253,13 @@ func migrate() {
 				baseExists = true
 			}
 
+			//If dependency was applied
 			if baseExists {
 				dev.LogInfo("Applying " + k.Name)
 				sqlstring := "BEGIN;"
+
+				//Targets contains all SQL Files wich are included in this migration
+				//Concat these here
 				for _, k := range k.Targets {
 					rawBytes, err := ioutil.ReadFile(cwd + "/db/migrations/" + k)
 
@@ -248,6 +267,9 @@ func migrate() {
 						dev.LogFatal(err, "Couldn't read target:", err.Error())
 					}
 
+					//Little fix to maybe catch some BEGIN; END; statements which break the transaction
+					//If BEGIN; / END; is specified in the SQL file, a single target file can fail and others be still applied.
+					//Thats not good because next start the whole migration will be applied again (even with the already applied target)
 					rawString := strings.ReplaceAll(strings.ReplaceAll(string(rawBytes), "BEGIN;", ""), "END;", "")
 
 					sqlstring += rawString
@@ -258,18 +280,22 @@ func migrate() {
 					dev.LogFatal(err, "Couldn't apply migration:", err.Error())
 				}
 
+				//Add Migration to applied patches (database and local)
 				if _, err := Connection.Exec(`INSERT INTO "Patches" VALUES ($1)`, k.Name); err != nil {
 					dev.LogFatal(err, "Couldn't apply migration:", err.Error())
 				}
 
 				appliedPatches = append(appliedPatches, k.Name)
 
+				//Removed this migration from neededPAtches
 				if len(neededPatches) >= i+2 {
 					neededPatches = append(neededPatches[:i], neededPatches[i+1:]...)
 				} else {
 					//No preceeding item left
 					neededPatches = make([]Migration, 0)
 				}
+
+				//Track how many migrations where applied in this run (multiple runs can occur because of dependencies)
 				patchesProcessed++
 			}
 		}
