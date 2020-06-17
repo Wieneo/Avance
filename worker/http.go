b5bf53e5c5a45ba7cdb087dd63cd1bc0bcd77943
Johann Gnaucke <johann@gnaucke.com>
2020-06-17 17:04:30 +0200

    Added SMTP check to /api/v1/health for workers

--- worker/http.go
+++ worker/http.go
@@ -2,8 +2,11 @@ package worker
 
 import (
 	//Imported to be used with database/sql
+	"crypto/tls"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"net/smtp"
 	"strconv"
 	"time"
 
@@ -49,6 +52,7 @@ func getInstanceHealth(w http.ResponseWriter, r *http.Request) {
 	var dummyDBVersion string
 
 	dBAlive := true
+	smtpAlive := true
 	err := db.Connection.QueryRow(`SELECT "Name" FROM "Patches" LIMIT 1`).Scan(&dummyDBVersion)
 	if err != nil {
 		dBAlive = false
@@ -58,11 +62,44 @@ func getInstanceHealth(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(500)
 	}
 
+	auth := smtp.PlainAuth("", config.CurrentConfig.SMTP.User, config.CurrentConfig.SMTP.Password, config.CurrentConfig.SMTP.Host)
+	// TLS config
+	tlsconfig := &tls.Config{
+		ServerName: config.CurrentConfig.SMTP.Host,
+	}
+
+	conn, err := tls.Dial("tcp", fmt.Sprintf("%s:%d", config.CurrentConfig.SMTP.Host, config.CurrentConfig.SMTP.Port), tlsconfig)
+	if err != nil {
+		dev.LogError(err, fmt.Sprintf("Couldn't connect to SMTP server: %s", err.Error()))
+		smtpAlive = false
+		w.WriteHeader(500)
+	} else {
+		c, err := smtp.NewClient(conn, config.CurrentConfig.SMTP.Host)
+		if err != nil {
+			dev.LogError(err, fmt.Sprintf("Couldn't connect to SMTP server: %s", err.Error()))
+			smtpAlive = false
+			w.WriteHeader(500)
+		} else {
+			c.StartTLS(tlsconfig)
+
+			// Auth
+			if err = c.Auth(auth); err != nil {
+				dev.LogError(err, fmt.Sprintf("Couldn't authenticate to SMTP server: %s", err.Error()))
+				smtpAlive = false
+				w.WriteHeader(500)
+			} else {
+				c.Close()
+			}
+		}
+	}
+
 	json.NewEncoder(w).Encode(struct {
 		DB     bool
+		SMTP   bool
 		Errors []string
 	}{
 		dBAlive,
+		smtpAlive,
 		errors,
 	})
 }
