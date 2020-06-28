c37fa6abca16d59641092319311ab096258f9ae8
Johann Gnaucke <johann@gnaucke.com>
2020-06-28 20:30:09 +0200

    Properties are now loading correctly

--- db/relations.go
+++ db/relations.go
@@ -30,7 +30,7 @@ func GetTicketRelations(TicketID int64) ([]models.Relation, error) {
 
 		if ticket1 == TicketID {
 			dev.LogDebug(fmt.Sprintf("[DB] [T: %d] Resolving related ticket %d", TicketID, ticket2))
-			ticket, _, err := GetTicketUnsafe(ticket2, models.WantedProperties{})
+			ticket, _, err := GetTicketUnsafe(ticket2, models.WantedProperties{Severity: true, Queue: true, Status: true, Owner: true})
 
 			if err != nil {
 				dev.LogDebug(fmt.Sprintf("[DB] [T: %d] Error getting related ticket %d -> Returning empty relations array: %s", TicketID, ticket2, err.Error()))
@@ -41,7 +41,7 @@ func GetTicketRelations(TicketID int64) ([]models.Relation, error) {
 			singleRelation.Type = relationType
 		} else {
 			dev.LogDebug(fmt.Sprintf("[DB] [T: %d] Resolving related ticket %d", TicketID, ticket1))
-			ticket, _, err := GetTicketUnsafe(ticket1, models.WantedProperties{})
+			ticket, _, err := GetTicketUnsafe(ticket1, models.WantedProperties{Severity: true, Queue: true, Status: true, Owner: true})
 
 			if err != nil {
 				dev.LogDebug(fmt.Sprintf("[DB] [T: %d] Error getting related ticket %d -> Returning empty relations array: %s", TicketID, ticket1, err.Error()))
